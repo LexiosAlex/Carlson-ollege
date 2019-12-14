6e36f1e54731b016ef1fba00ebce8a2025f42fc0
LexiosAlex <lexiosma@gmail.com>
2019-12-14 04:13:39 +0300

    added pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-// import './App.css';
+import './App.css';
+
+import Home from './pages/Home.js'
+import Rooms from './pages/Rooms.js'
+import SingleRoom from './pages/SingleRoom.js'
+import Error from './pages/Error.js'
+
 
 function App() {
   return (
---
+++ src/pages/Error.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>hello from error page</div>;
+};
+
+export default Home;
